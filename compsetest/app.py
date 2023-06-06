7dbb4f8a1dadc3f1fd5febfcf81fcc58453676c6
rininafuku <salt.end.lemon@gmail.com>
2023-06-06 23:00:12 +0900

    新規ファイルとディレクトリを追加

---
+++ compsetest/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+import redis
+
+app = Flask(__name__)
+cache = redis.Redis(host ='redis',port=6379)
+
+
+@app.route('/')
+def index():
+    count = cache.incr('hits')
+    return 'Hello world! I have seen  {} times\n.'.format(count)
